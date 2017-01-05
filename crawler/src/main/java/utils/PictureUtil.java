515a7d939d413e5ae152e211e1477d3030ddfaf5
LiMengxian <imxlee@foxmail.com>
2017-01-05 21:17:49 +0800

    update

--- crawler/src/main/java/utils/PictureUtil.java
+++ crawler/src/main/java/utils/PictureUtil.java
@@ -5,14 +5,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +42,8 @@ public class PictureUtil {
 		cm.setDefaultMaxPerRoute(4);
 		builder.setConnectionManager(cm);
 		// 获取一个HttpClient对象，模拟浏览器
-		CloseableHttpClient httpclient = builder.build();
+		// CloseableHttpClient httpclient = builder.build();
+		CloseableHttpClient httpclient = createSSLInsecureClient(builder);
 		HttpGet httpget = new HttpGet(url);
 		httpget.setHeader("User-Agent",
 				"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36");
@@ -76,5 +84,25 @@ public class PictureUtil {
 				logger.error("资源释放失败" + e.getMessage());
 			}
 		}
+	}
+
+	// client工具函数，信任对方（https）所有证书
+	private static CloseableHttpClient createSSLInsecureClient(HttpClientBuilder builder) {
+		try {
+			// 创建安全套接字对象
+			SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+				// 信任所有
+				public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+					return true;
+				}
+			}).build();
+			// 获取分层tls/ssl连接
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, new String[] { "SSLv3" },
+					null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+			return builder.setSSLSocketFactory(sslsf).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return HttpClients.createDefault();
 	}
 }
